c686f26b04ea1e43fcbce17748ff52464c36bf90
cristianfabrega08 <cristian.fabrega@usm.cl>
2023-09-26 22:10:28 -0300

    tablas

---
+++ database/migrations/2023_09_26_185802_create_mascota_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mascota', function (Blueprint $table) {
+            $table->primary('cod_mascota');
+            $table->integer('codigo_servicio');
+            $table->string('nom_mascota');
+            $table->string('rut_cliente');
+            $table->timestamps();
+            $table->foreign('rut_cliente')->references('rut_cliente')->on('cliente');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mascota');
+    }
+};
