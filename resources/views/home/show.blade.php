58a423f49d9f42ddab8a33eb8e9604bbdf1f15d5
GonzaloCastillo1483 <gonzalo.castillo1483@gmail.com>
2023-09-27 22:34:35 -0300

    aaaaaaaaaa

--- resources/views/home/show.blade.php
+++ resources/views/home/show.blade.php
@@ -2,47 +2,70 @@
 
 @section('contenido')
 
-<body style="background: linear-gradient(to bottom, #50308a 0%, #c9676f 100%);">
+<body style="background-color: lightblue">
     <div class="container-fluid d-flex flex-column justify-content-lg-center">
-        <div class="row">
-            <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6">
-                <div id="thegoodpets" class="carousel slide"  style="width: 650px">
+        <div class="row ">
+            <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6 mt-5 justify-content-lg-center">
+                <div id="carrusel1" class="carousel slide"  style="width: 650px">
                     
+                    
+
+
+                  <div class="container-fluid">
+                    <div id="carouselExampleRide" class="carousel slide" data-bs-ride="true">
                     <div class="carousel-inner">
-                      <div class="carousel-item active">
-                        <img src="{{asset('images/shawshaw.jpg')}}" class="d-block w-100" alt="messi">
-                      </div>
-                      <div class="carousel-item ">
-                        <img src="{{asset('images/pelu.jpg')}}" class="d-block w-100" alt="pelusa">
-                      </div>
-                      <div class="carousel-item ">
-                        <img src="{{asset('images/booz.jpg')}}" class="d-block w-100" alt="booz">
-                      </div>
-                      <div class="carousel-item ">
-                        <img src="{{asset('images/akita.jpg')}}" class="d-block w-100" alt="akita">
-                      </div>
-                      <div class="carousel-item ">
-                        <img src="{{asset('images/luna.jpg')}}" class="d-block w-100" alt="luna">
-                      </div>
-                      <div class="carousel-item ">
-                        <img src="{{asset('images/nose.jpg')}}" class="d-block w-100" alt="bartolo">
-                      </div>
-                      <div class="carousel-item ">
-                        <img src="{{asset('images/sofy.jpg')}}" class="d-block w-100" alt="sofy">
-                      </div>
-                      <div class="carousel-item ">
-                        <img src="{{asset('images/toby.jpg')}}" class="d-block w-100" alt="toby">
-                      </div>
+                        <div class="carousel-item active">
+                        <img src="{{asset('images/shawshaw.jpg')}}" class="d-block d-flex img-fluid flex-fill imagen-banner" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                        <img src="{{asset('images/pelu.jpg')}}" class="d-block d-flex img-fluid flex-fill imagen-banner" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                        <img src="{{asset('images/booz.jpg')}}" class="d-block d-flex img-fluid flex-fill imagen-banner" alt="...">
+                        </div>
+
+                        <div class="carousel-item">
+                          <img src="{{asset('images/akita.jpg')}}" class="d-block d-flex img-fluid flex-fill imagen-banner" alt="...">
+                        </div>
+
+                        <div class="carousel-item">
+                          <img src="{{asset('images/luna.jpg')}}" class="d-block d-flex img-fluid flex-fill imagen-banner" alt="...">
+                        </div>
+
+                        <div class="carousel-item">
+                          <img src="{{asset('images/nose.jpg')}}" class="d-block d-flex img-fluid flex-fill imagen-banner" alt="...">
+                        </div>
+
+                        <div class="carousel-item">
+                          <img src="{{asset('images/sofy.jpg')}}" class="d-block d-flex img-fluid flex-fill imagen-banner" alt="...">
+                        </div>
+
+                        <div class="carousel-item">
+                          <img src="{{asset('images/toby.jpg')}}" class="d-block d-flex img-fluid flex-fill imagen-banner" alt="...">
+                        </div>
+
+
+
+
+
+
+
+
+                          
+
+
+
                     </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-                      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                      <span class="visually-hidden">Anterior</span>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleRide" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
                     </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-                      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                      <span class="visually-hidden">siguiente</span>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleRide" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
                     </button>
-                  </div>
+                    </div>
+                </div>
             </div>
            
         </div>
